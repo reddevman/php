0176e9dfe2e4e834c5041c6ddab159c217fcf1c6
Alex <pruebasgit@prueba.com>
2020-10-31 14:00:20 +0100

    Métodos para convertir más monedas

--- Formularios/-Forms-CasoPractico3-ConvMoneda/conversion.php
+++ Formularios/-Forms-CasoPractico3-ConvMoneda/conversion.php
@@ -1,29 +1,87 @@
 <?php
-    class Conversion {
-        private $euros;
+class Conversion
+{
+    private $euros;
+    private $libras;
+    private $dolares;
 
-        /**
-         * Get the value of euros
-         */ 
-        public function getEuros()
-        {
-            return $this->euros;
-        }
-
-        /**
-         * Set the value of euros
-         *
-         * @return  self
-         */ 
-        public function setEuros($euros)
-        {
-            $this->euros = $euros;
-        }
-
-
-        public function convertir ($euros) {
-            $conversion = 0.9;
-            $conversion *= $euros;
-            return $conversion;
-        }
+    /**
+     * Get the value of euros
+     */
+    public function getEuros()
+    {
+        return $this->euros;
     }
+
+    /**
+     * Set the value of euros
+     *
+     * @return  self
+     */
+    public function setEuros($euros)
+    {
+        $this->euros = $euros;
+    }
+
+    /**
+     * Get the value of libras
+     */
+    public function getLibras()
+    {
+        return $this->libras;
+    }
+
+    /**
+     * Set the value of libras
+     *
+     * @return  self
+     */
+    public function setLibras($libras)
+    {
+        $this->libras = $libras;
+    }
+
+    /**
+     * Get the value of dolares
+     */
+    public function getDolares()
+    {
+        return $this->dolares;
+    }
+
+    /**
+     * Set the value of dolares
+     *
+     * @return  self
+     */
+    public function setDolares($dolares)
+    {
+        $this->dolares = $dolares;
+    }
+
+
+    public function convEurosLibras($euros)
+    {
+        $conversion = 0.9;
+        $conversion *= $euros;
+        return $conversion;
+    }
+    public function convLibrasEuros($libras)
+    {
+        $conversion = 1.11;
+        $conversion *= $libras;
+        return $conversion;
+    }
+    public function convEurosDolares($euros)
+    {
+        $conversion = 1.17;
+        $conversion *= $euros;
+        return $conversion;
+    }
+    public function convDolaresEuros($dolares)
+    {
+        $conversion = 0.86;
+        $conversion *= $dolares;
+        return $conversion;
+    }
+}
--- Formularios/-Forms-CasoPractico3-ConvMoneda/envioformulario.php
+++ Formularios/-Forms-CasoPractico3-ConvMoneda/envioformulario.php
@@ -5,18 +5,22 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
         integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="style.css">
     <title>Document</title>
 </head>
 <body>
-    <div class="container">
+    <div class="container formulario">
+        <h1>La conversión es:</h1>
         <?php
+
+        # if (isset($_POST["X"] && !empty($_POST["X"])){}
             require 'conversion.php';
 
             $eurosLibras = new Conversion ();
             $eurosLibras->setEuros($_POST["euros"]);
 
-            echo 'La conversión de ' . $eurosLibras->getEuros() . '€ a Libras es de: ' . $eurosLibras->convertir($eurosLibras->getEuros());
+            echo 'La conversión de ' . $eurosLibras->getEuros() . '€ a Libras es de: ' . $eurosLibras->convEurosLibras($eurosLibras->getEuros()) . ".";
         ?>
     </div>
 </body>
