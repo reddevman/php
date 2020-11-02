279d1481cc775340c2de4b61f4ff395cad924478
Alex <pruebasgit@prueba.com>
2020-11-02 19:09:44 +0100

    Tabla insertada

--- Ejercicios 30-09/Ejercicio5.php
+++ Ejercicios 30-09/Ejercicio5.php
@@ -1,8 +1,24 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tablas de multiplicar</title>
+</head>
+<body>
+    <table border = "1">
 <?php
 // Mostrar las tablas de multiplicar del 1 al 10. Utilizar bucle for.
     for ($i=1; $i <= 10 ; $i++) {
+        echo '<tr>';
         for ($j=1; $j <= 10 ; $j++) {
-            echo $i . "*" . $j . "=" . $i*$j . "<br>";
+            echo '<td>';
+            echo $i . "*" . $j . "=" . $i*$j;
+            echo '</td>';
         }
+        echo '</tr>';
     }
 ?>
+</table>
+</body>
+</html>
