89a5ece08e19e0f87fd36df2632d7fc9b96eb786
reddevman <alemarqred@gmail.com>
2020-11-04 14:02:03 +0100

    Modificados estilos

--- PHP-MYSQL/CasoPractico3/index.php
+++ PHP-MYSQL/CasoPractico3/index.php
@@ -4,14 +4,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="style.css" type="text/css">
     <title>Base de Datos PHP Beneficios</title>
 </head>
 
 <body>
     <header>
-        <p>Caso Práctico 3</p>
-        <p>Base de datos Beneficios</p>
+        <div id="wrap-title">
+            <ul>
+                <li>Caso Práctico 3</li>
+                <li>Base de datos Beneficios</li>
+            </ul>
+        </div>
         <nav>
             <ul>
                 <li><a href="total.php">Total de ventas</a></li>
