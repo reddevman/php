c0b9cc9a5b0a2bdecff419ba1e4e317d4c928d89
Alex <pruebasgit@prueba.com>
2020-11-11 10:22:04 +0100

    Rellenado alumno.php

---
+++ MVC/Ejercicio01/alumno.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Base de datos Alumnos</title>
+</head>
+<body>
+    <form action="lib/escuela.php" method="post">
+    <fieldset>
+        <legend>Insertar un alumno</legend>
+        <label for="nombre">Nombre:</label><br>
+        <input type="text" name="nombre"><br>
+        <label for="nombre">Apellidos:</label><br>
+        <input type="text" name="apellidos"><br>
+        <label for="nombre">edad:</label><br>
+        <input type="text" name="edad">
+    </fieldset>
+    </form>
+</body>
+</html>
--- MVC/Ejercicio01/lib/escuela.php
+++ MVC/Ejercicio01/lib/escuela.php
@@ -1,31 +1,44 @@
 <?php
     class Escuela 
     {
+        # Creación de variables para cada parámetro de la conexión SQL
         private $host = "localhost";
         private $user = "root";
         private $pass = "";
         private $db_name = "escuela";
 
-        private $mysqli;
+        # Propiedad mysqli que tendrá la conexión a la base de datos
+        private $conn;
+        # Variable de error si la conexión no se hiciera efectiva
         private $error = false;
 
         function __construct()
         {
-            $this->mysqli = new mysqli($this->host, $this->user, $this->pass, $this->db_name);
-            if ($this->mysqli->connect_errno) {
+            $this->conn = new mysqli($this->host, $this->user, $this->pass, $this->db_name);
+            if ($this->conn->connect_errno) {
                 $this->error = true;
             }
         }
 
+        # Función que devuelve la consulta a una variable para usar en otro archivo php
         public function devolverAlumnos()
         {
             if (!$this->error) {
-                $resultado = $this->mysqli->query("SELECT nombre, apellidos, edad FROM alumnos");
+                $resultado = $this->conn->query("SELECT nombre, apellidos, edad FROM alumnos");
                 return $resultado;
-            } else {
+
+            } else { # Se usa el valor null para luego reutilizarlo como comprobación en el while de mostrar la consulta
                     return null;
             }
         }
-    }
 
+        public function insertarAlumno($nombre, $apellidos, $edad)
+        {
+            # Se crea la variable que contiene la sentencia de inserción de un alumno
+            $insertarSql = "INSERT INTO alumnos (nombre,apellidos,edad) VALUES ('" . $nombre . "', '" . $apellidos . "',
+            " . $edad . ")";
+            # Asignamos al query la consulta de la conexión que realicemos la variable que contiene la sentencia de sql
+            $this->conn->query($insertarSql);
+        }
+    }
 ?>
