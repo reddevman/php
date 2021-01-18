71fc2d1efe7f76f04e4a774b819d9a67cb0d2211
Alex <pruebasgit@prueba.com>
2021-01-18 21:34:19 +0100

    tarea 1 sesiones

--- Sesiones y cookies/Tarea 1/index.php
+++ Sesiones y cookies/Tarea 1/index.php
@@ -1,27 +1,53 @@
+<?php
+session_start();
+?>
+
 <!DOCTYPE html>
 <html lang="es">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;300;400;500;700&display=swap"
+        rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
     <title>Tarea 1 SESIONES</title>
 </head>
 
 <body>
-    <?php
-    include "mostrar.php";
-    session_start();
-    if (isset($inputSessionName) && !empty($inputSessionName)) {
-        $inputSessionName = $_REQUEST['nombre'];
-    } else{
-        $inputSessionName = [];
-    } 
-    ?>
-    <form action="mostrar.php" method="post">
-        <label for="nombre">Introduce un nombre:</label>
-        <input type="text" name="nombre" id="nombre">
-        <input type="submit" value="Enviar">
-    </form>
+    <div class="main-wrapper">
+        <h1>2º DAW - SESIONES Y COOKIES - TAREA 1</h1>
+        <div class="formulario">
+            <form action="mostrar.php" method="post">
+                <fieldset>
+                    <legend>SESIONES DE USUARIO</legend>
+                    <label for="nombre">Introduce un nombre:</label>
+                    <input type="text" name="nombre" id="nombre">
+                    <input class="subButton" type="submit" value="Enviar">
+                </fieldset>
+            </form>
+        </div>
+        <div class="results">
+            <?php
+            if (isset($_SESSION['nombre']) && !empty($_SESSION['nombre'])) {
+                echo "Usuario loggeado como:<b> " .  $_SESSION['nombre'] . "</b>";
+                echo "<br>";
+                echo "Recarga la página para destruir la sesión.";
+                session_destroy();
+            } else {
+                echo "Introduce un nombre para iniciar sesión :)";
+            }
+            // if (isset($_GET['nombre']) && !empty($_GET['nombre'])) {
+            //     $_SESSION['nombre'] = $_GET['nombre'];
+            //     echo $_SESSION['nombre'];
+            // } else {
+            //     $_SESSION['nombre'] = null;
+            // }
+            ?>
+        </div>
+    </div>
+    <footer>&copy; ALEJANDRO MÁRQUEZ ARAGONÉS</footer>
 </body>
 
 </html>
--- Sesiones y cookies/Tarea 1/mostrar.php
+++ Sesiones y cookies/Tarea 1/mostrar.php
@@ -1,18 +1,32 @@
+<?php
+session_start();
+?>
+
 <!DOCTYPE html>
 <html lang="es">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;300;400;500;700&display=swap"
+        rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" />
     <title>Tarea 1 SESIONES</title>
 </head>
 
 <body>
     <?php
-    $inputSessionName = $_POST['nombre'];
-    echo $inputSessionName;
-    echo "<a href='index.php?nombre=" . $inputSessionName . "'>ATRÁS</a>";
-?>
+    if (isset($_POST['nombre']) && !empty($_POST['nombre'])) {
+        $_SESSION['nombre'] = $_POST['nombre'];
+        echo "Usuario '<b>" . $_SESSION['nombre'] . "<b>'";
+    } else {
+        echo "No se ha introducido ningún nombre.";
+    }
+    ?>
+    <br>
+    <a href="index.php"><i class="fas fa-arrow-left"></i>ATRÁS</a>
+    <footer>&copy; ALEJANDRO MÁRQUEZ ARAGONÉS</footer>
 </body>
 
 </html>
