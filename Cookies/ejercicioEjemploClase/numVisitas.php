5dc6ac08f7b8b25ff349a0c9cac90a3bd2dd954e
reddevman <alemarqred@gmail.com>
2021-01-18 13:38:58 +0100

    ejemplo de cookies

---
+++ Cookies/ejercicioEjemploClase/numVisitas.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Cookies</title>
+</head>
+
+<body>
+    <?php
+  if (!isset($_COOKIE['numVisitas'])) {
+    setcookie('numVisitas', 0);
+    echo "Cookie generada";
+  } else {
+    $valor = $_COOKIE['numVisitas'] + 1;
+    setcookie('numVisitas', $valor);
+  }
+
+  echo "Número de visitas = " . $_COOKIE['numVisitas'];
+  ?>
+</body>
+
+</html>
